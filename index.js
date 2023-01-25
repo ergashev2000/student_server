24f7757d036a58389b72b7e0d5a5cfae37191385
Islomjon <e.islom@abutech.uz>
2023-01-25 15:39:30 +0500

    server header

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const cors = require('cors');
 const pool = require('./config/index')
 const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
