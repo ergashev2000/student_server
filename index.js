cdfcc5eb59507ab8d07b58d334c1c17784a3afd0
Islomjon <e.islom@abutech.uz>
2023-01-26 12:57:54 +0500

    add put and delete

--- index.js
+++ index.js
@@ -30,18 +30,40 @@ app.get('/students', (req, res) => {
 
 app.post('/students', (req, res) => {
     const { firstname, lastname, age, group } = req.body;
-    console.log(req.body);
-    if (!firstname) {
-        return res.status(400).send({ message: req.body });
-    }
     pool.query('INSERT INTO students (firstname, lastname, age, "group") VALUES ($1, $2, $3, $4)', [firstname, lastname, age, group], (error, result) => {
         if (error) {
             res.status(400).send(error);
         } else {
-            res.status(201).send({ message: 'User added successfully'});
+            res.status(201).send({ message: req.body});
         }
     });
 });
+
+
+app.put('/students/:id', (req, res) => {
+    const studentId = req.params.id;
+    const { firstname, lastname, age, group } = req.body;
+    pool.query('UPDATE students SET firstname = $1, lastname = $2, age = $3, "group" = $4 WHERE id = $5', [firstname, lastname, age, group, studentId], (error, result) => {
+        if (error) {
+            res.status(400).send(error);
+        } else {
+            res.status(200).send({ message: 'Student updated successfully'});
+        }
+    });
+});
+
+
+app.delete('/students/:id', (req, res) => {
+    const studentId = req.params.id;
+    pool.query('DELETE FROM students WHERE id = $1', [studentId], (error, result) => {
+        if (error) {
+            res.status(400).send(error);
+        } else {
+            res.status(200).send({ message: 'Student deleted successfully' });
+        }
+    });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
